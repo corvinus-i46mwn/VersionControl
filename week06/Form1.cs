2931b3ba94050f0ea2b31e02bba4b2e10eaff38d
Ambrus Dániel Kollin <ambrus.kollin@stud.uni-corvinus.hu>
2021-10-28 16:22:22 +0200

    week06-6

--- week06/Form1.cs
+++ week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week06.Entitites;
 using week06.MnbServiceReference;
@@ -22,6 +23,7 @@ namespace week06
             KulonFuggveny();
             
             dataGridView1.DataSource = Rates;
+            CreateChart();
         }
         void KulonFuggveny()
         {
@@ -63,6 +65,23 @@ namespace week06
                     rate.Value = value / unit;
             }
         }
-        
+        void CreateChart()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
     }
 }
