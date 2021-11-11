c60310d8cd1f279e534638b4bd09f2788f11d81c
Ambrus Dániel Kollin <ambrus.kollin@stud.uni-corvinus.hu>
2021-11-11 13:30:24 +0100

    week08-done

--- week08/Form1.Designer.cs
+++ week08/Form1.Designer.cs
@@ -30,12 +30,141 @@ namespace week08
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.createTimer = new System.Windows.Forms.Timer(this.components);
+            this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.carButton = new System.Windows.Forms.Button();
+            this.ballButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorPick = new System.Windows.Forms.Button();
+            this.presentButton = new System.Windows.Forms.Button();
+            this.ribbonColorPick = new System.Windows.Forms.Button();
+            this.boxColorPick = new System.Windows.Forms.Button();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.boxColorPick);
+            this.mainPanel.Controls.Add(this.ribbonColorPick);
+            this.mainPanel.Controls.Add(this.presentButton);
+            this.mainPanel.Controls.Add(this.colorPick);
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.ballButton);
+            this.mainPanel.Controls.Add(this.carButton);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(800, 450);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // createTimer
+            // 
+            this.createTimer.Enabled = true;
+            this.createTimer.Interval = 3000;
+            this.createTimer.Tick += new System.EventHandler(this.createTimer_Tick);
+            // 
+            // conveyorTimer
+            // 
+            this.conveyorTimer.Enabled = true;
+            this.conveyorTimer.Interval = 10;
+            this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
+            // 
+            // carButton
+            // 
+            this.carButton.Location = new System.Drawing.Point(3, 334);
+            this.carButton.Name = "carButton";
+            this.carButton.Size = new System.Drawing.Size(108, 49);
+            this.carButton.TabIndex = 0;
+            this.carButton.Text = "CAR";
+            this.carButton.UseVisualStyleBackColor = true;
+            this.carButton.Click += new System.EventHandler(this.carButton_Click);
+            // 
+            // ballButton
+            // 
+            this.ballButton.Location = new System.Drawing.Point(3, 389);
+            this.ballButton.Name = "ballButton";
+            this.ballButton.Size = new System.Drawing.Size(108, 49);
+            this.ballButton.TabIndex = 1;
+            this.ballButton.Text = "BALL";
+            this.ballButton.UseVisualStyleBackColor = true;
+            this.ballButton.Click += new System.EventHandler(this.ballButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coming next....";
+            // 
+            // colorPick
+            // 
+            this.colorPick.BackColor = System.Drawing.Color.Fuchsia;
+            this.colorPick.Location = new System.Drawing.Point(117, 389);
+            this.colorPick.Name = "colorPick";
+            this.colorPick.Size = new System.Drawing.Size(87, 23);
+            this.colorPick.TabIndex = 3;
+            this.colorPick.UseVisualStyleBackColor = false;
+            this.colorPick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // presentButton
+            // 
+            this.presentButton.Location = new System.Drawing.Point(3, 279);
+            this.presentButton.Name = "presentButton";
+            this.presentButton.Size = new System.Drawing.Size(108, 49);
+            this.presentButton.TabIndex = 4;
+            this.presentButton.Text = "PRESENT";
+            this.presentButton.UseVisualStyleBackColor = true;
+            this.presentButton.Click += new System.EventHandler(this.presentButton_Click);
+            // 
+            // ribbonColorPick
+            // 
+            this.ribbonColorPick.BackColor = System.Drawing.Color.Fuchsia;
+            this.ribbonColorPick.Location = new System.Drawing.Point(117, 279);
+            this.ribbonColorPick.Name = "ribbonColorPick";
+            this.ribbonColorPick.Size = new System.Drawing.Size(87, 23);
+            this.ribbonColorPick.TabIndex = 5;
+            this.ribbonColorPick.UseVisualStyleBackColor = false;
+            this.ribbonColorPick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // boxColorPick
+            // 
+            this.boxColorPick.BackColor = System.Drawing.Color.Lime;
+            this.boxColorPick.Location = new System.Drawing.Point(117, 305);
+            this.boxColorPick.Name = "boxColorPick";
+            this.boxColorPick.Size = new System.Drawing.Size(87, 23);
+            this.boxColorPick.TabIndex = 6;
+            this.boxColorPick.UseVisualStyleBackColor = false;
+            this.boxColorPick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Timer createTimer;
+        private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ballButton;
+        private System.Windows.Forms.Button carButton;
+        private System.Windows.Forms.Button colorPick;
+        private System.Windows.Forms.Button boxColorPick;
+        private System.Windows.Forms.Button ribbonColorPick;
+        private System.Windows.Forms.Button presentButton;
     }
 }
 
--- week08/Form1.cs
+++ week08/Form1.cs
@@ -7,14 +7,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week08.Abstractions;
+using week08.Entitites;
 
 namespace week08
 {
     public partial class Form1 : Form
     {
+        private List<Toy> _toys = new List<Toy>();
+        private Toy _nextToy;
+
+        private IToyFactory _factory;
+        public IToyFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value;
+                DisplayNext();
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            //Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            if (Factory == null) return;
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var toy in _toys)
+            {
+                toy.MoveToy();
+                if (toy.Left > maxPosition)
+                    maxPosition = toy.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestToy = _toys[0];
+                mainPanel.Controls.Remove(oldestToy);
+                _toys.Remove(oldestToy);
+            }
+        }
+
+        private void carButton_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void ballButton_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory
+            {
+                BallColor = colorPick.BackColor
+            };
+        }
+        void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+        }
+
+        private void presentButton_Click(object sender, EventArgs e)
+        {
+            Factory = new PresentFactory
+            {
+                BoxColor = boxColorPick.BackColor,
+                RibbonColor=ribbonColorPick.BackColor
+            };
         }
     }
 }
